1a8368de764fae6c1661c504951e4b1297d67ee8
youzhang <guitar2009king@gmail.com>
2014-07-07 20:57:42 +0800

    search place in  baidu map

---
+++ webInterface/baiduMap.py
@@ -0,0 +1,8 @@
+from baidumap import xBaiduMap
+bm=xBaiduMap()
+print bm.getLocation("红旗大街淮河路",'哈尔滨')
+print bm.getLocation("人民广场",'上海')
+print bm.getLocation("人民路沙浦路",'广州')
+print bm.getLocation("炼石水泥厂",'福建')
+print bm.getAddress(26.798127,118.801387)
+print bm.getAddress(30.690714,104.079473)
